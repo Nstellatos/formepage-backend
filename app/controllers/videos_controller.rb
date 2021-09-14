3caa9a299f39e42784d5dae91b8a7fb9bebbdb09
Nicole Stellatos <n_stellatos@yahoo.com>
2021-09-14 18:36:00 -0400

    video create method test

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,4 +3,17 @@ class VideosController < ApplicationController
         videos = Video.all 
         render json: videos
     end
+    def create 
+        video = Video.create(video_params)
+        url = params[:video][:video_url]
+        url = url.match(/(?<=video\/)\d{10,}/)
+        video.video_url = url
+        render json: video 
+    end
+
+    private
+    def video_params 
+        params.require(:video).permit(:video_url)
+    end
 end
+
