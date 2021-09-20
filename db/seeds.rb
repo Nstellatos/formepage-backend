009ef9767db3089985f92bb2743f915ab0edf15b
Nicole Stellatos <n_stellatos@yahoo.com>
2021-09-20 17:35:27 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-laughs = Category.create(name: "Laughs")
+laughs = Category.create(name: "laughs")
+makeup = Category.create(name: "makeup")
+recipe = Category.create(name: "recipe")
 
 
-Video.create(video_url: "https://www.tiktok.com/@brussellham/video/7000420900419980549?sender_device=mobile&sender_web_id=7007507174352111109&is_from_webapp=v1&is_copy_url=0", category_id: laughs.id )
-Video.create(video_url: "https://www.tiktok.com/@joerauth_/video/7006352635620117765?sender_device=mobile&sender_web_id=7007507174352111109&is_from_webapp=v1&is_copy_url=0")
-
+Video.create(video_url: "https://www.tiktok.com/@brussellham/video/7000420900419980549?sender_device=mobile&sender_web_id=7007507174352111109&is_from_webapp=v1&is_copy_url=0", category_id: laughs.id)
+Video.create(video_url: "https://www.tiktok.com/@joerauth_/video/7006352635620117765?sender_device=mobile&sender_web_id=7007507174352111109&is_from_webapp=v1&is_copy_url=0", category_id: laughs.id)
+Video.create(video_url: "https://www.tiktok.com/@adisonjustis/video/7009062940837989638?sender_device=mobile&sender_web_id=7007507174352111109&is_from_webapp=v1&is_copy_url=0", category_id: makeup.id)
+Video.create(video_url: "https://www.tiktok.com/@bysophialee/video/7009830297285987590?sender_device=mobile&sender_web_id=7007507174352111109&is_from_webapp=v1&is_copy_url=0", category_id: recipe.id)
